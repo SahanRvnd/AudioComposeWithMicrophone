11784b7de97a38e2beed6e85613a610d9be5e0bc
Sahan Ravindu <sahanrvnd@gmail.com>
2022-04-05 08:36:49 +0530

    Add Unit test

--- AudioComposeWithMicrophone/ViewController.swift
+++ AudioComposeWithMicrophone/ViewController.swift
@@ -6,14 +6,119 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    @IBOutlet weak var btnRecord: UIButton!
+    @IBOutlet weak var btnPlay: UIButton!
+    
+    var recordingSession: AVAudioSession!
+    var whistleRecorder: AVAudioRecorder!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        self.loadRecordingUI()
+                    } else {
+                        self.loadFailUI()
+                    }
+                }
+            }
+        } catch {
+            self.loadFailUI()
+        }
+    }
+    
+    @IBAction func didClickRecord(_ sender: Any) {
+        if whistleRecorder == nil {
+            startRecording()
+        } else {
+            finishRecording(success: true)
+        }
+    }
+    
+    @IBAction func didClickPlay(_ sender: Any) {
+    }
+    
+    func loadRecordingUI() {
     }
 
+    func loadFailUI() {
+    }
+    
+    func startRecording() {
+        // 1
+        view.backgroundColor = UIColor(red: 0.6, green: 0, blue: 0, alpha: 1)
 
+        // 2
+        btnRecord.setTitle("Tap to Stop", for: .normal)
+
+        // 3
+        let audioURL = ViewController.getWhistleURL()
+        print(audioURL.absoluteString)
+
+        // 4
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+
+        do {
+            // 5
+            whistleRecorder = try AVAudioRecorder(url: audioURL, settings: settings)
+            whistleRecorder.delegate = self
+            whistleRecorder.record()
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    class func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+
+    class func getWhistleURL() -> URL {
+        return getDocumentsDirectory().appendingPathComponent("whistle.m4a")
+    }
+    
+    func finishRecording(success: Bool) {
+        view.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0, alpha: 1)
+
+        whistleRecorder.stop()
+        whistleRecorder = nil
+
+        if success {
+            btnRecord.setTitle("Tap to Re-record", for: .normal)
+            //navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(nextTapped))
+        } else {
+            btnRecord.setTitle("Tap to Record", for: .normal)
+
+            let ac = UIAlertController(title: "Record failed", message: "There was a problem recording your whistle; please try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
+    }
+    
 }
+
 
