1056b572ae0dc5be268a5da9301b4fa18e65e6ac
Sahan Ravindu <sahanrvnd@gmail.com>
2022-04-05 12:06:13 +0530

    Correct test logic

--- AudioComposeWithMicrophoneTests/AudioComposeWithMicrophoneTests.swift
+++ AudioComposeWithMicrophoneTests/AudioComposeWithMicrophoneTests.swift
@@ -21,7 +21,7 @@ class AudioComposeWithMicrophoneTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
-        let i = 1
+        let i = 0
         
         XCTAssert(i == 0, "i supporst to be 0")
     }
